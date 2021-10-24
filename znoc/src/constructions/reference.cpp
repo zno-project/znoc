5db24e80e7bb116450981b8d1dcf68b96698e0b5
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2021-10-24 22:10:25 +0100

    Update reference parsing to allow calling member functions Note: passes `self` in a semi-horrible way

--- znoc/src/constructions/reference.cpp
+++ znoc/src/constructions/reference.cpp
@@ -10,6 +10,7 @@
 #include "../memory/memory_ref.hpp"
 #include "../memory/gep.hpp"
 #include "binary_op.hpp"
+#include "unary_op.hpp"
 #include "construction_parse.hpp"
 #include "function_call.hpp"
 #include "../parsing.hpp"
@@ -34,14 +35,27 @@ std::unique_ptr<AST::Expression> Parser::parse_identifier_expression(FILE* f) {
 
 		return std::make_unique<AST::FunctionCall>(parsed_namespace.parsed_namespace->get_function_by_name(func_name), std::move(args));
 	} else {
-		std::cout << "Variable reference - ignoring namespaces" << std::endl;
-		std::cout << "Variable reference - current tok is " << currentToken << std::endl;
+		std::cout << "Variable reference or member func - ignoring namespaces" << std::endl;
 		std::shared_ptr<AST::MemoryLoc> variable = AST::get_var(parsed_namespace.next_identifier);
 
 		UNTIL_NOT('.', {
 			auto field_name = EXPECT_IDENTIFIER("field name");
-			auto field_info = variable->underlying_type.get_field_info_by_name(field_name);
-			variable = std::make_unique<AST::GEP>(variable, field_info.index);
+			if (currentToken == '(') {
+				// Call to member function
+				std::vector<std::unique_ptr<AST::Expression>> args;
+				// TODO: This could probably be improved
+				args.push_back(std::make_unique<AST::UnaryExpression>('&', std::make_unique<AST::MemoryRef>(variable)));
+
+				LIST('(', ',', ')', {
+					args.push_back(parse_binary_expression(f));
+				}, "function args");
+
+				return std::make_unique<AST::FunctionCall>(variable->underlying_type.get_function_by_name(field_name), std::move(args));
+			} else {
+				// Variable ref
+				auto field_info = variable->underlying_type.get_field_info_by_name(field_name);
+				variable = std::make_unique<AST::GEP>(variable, field_info.index);
+			}
 		});
 
 		return std::make_unique<AST::MemoryRef>(variable);
