28c16391e6e10bee91fd5c655871ba3668637dab
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2021-10-28 16:46:08 +0100

    - Fix destructor - Properly init and reset all globals in test framework

--- znoc/test/unit/framework.hpp
+++ znoc/test/unit/framework.hpp
@@ -3,6 +3,8 @@
 #include "types/type.hpp"
 #include "constructions/namespace.hpp"
 #include "parsing.hpp"
+#include "location.hpp"
+#include "constructions/expression.hpp"
 
 #include <deque>
 #include <map>
@@ -12,10 +14,13 @@
 #include <string.h>
 
 std::unique_ptr<AST::Namespace> GlobalNamespace;
+extern std::map<FILE*, char> lastChars;
 
 class CompilerGlobals {
  public:
   CompilerGlobals() {
+		LexLoc = {1, 0};
+		lastChars = std::map<FILE*, char>();
 		stack_allocations.push_front(std::map<std::string, std::shared_ptr<AST::MemoryLoc>>());
 		GlobalNamespace = std::make_unique<AST::Namespace>("_G");
 		AST::init_builtin_types();
@@ -24,6 +29,8 @@ class CompilerGlobals {
   virtual ~CompilerGlobals() {
 	  stack_allocations.clear();
 	  GlobalNamespace = nullptr;
+	  mergeBB = nullptr;
+	  condBB = nullptr;
   }
 
   // void TearDown() override {}
@@ -43,7 +50,7 @@ class CompilerMain: CompilerGlobals {
 		get_next_token(f);
   }
 
-  virtual CompilerMain() {
+  virtual ~CompilerMain() {
 	  fclose(f);
   };
 
