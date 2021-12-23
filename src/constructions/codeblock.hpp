2d9885909451f3612b9767a21b1afa1a6d85d092
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2021-12-23 15:35:01 +0000

    Add basic constexpr codegen of code blocks

--- src/constructions/codeblock.hpp
+++ src/constructions/codeblock.hpp
@@ -9,6 +9,8 @@
 #include <llvm/IR/Value.h>
 #include <llvm/IR/IRBuilder.h>
 
+#include <iostream>
+
 namespace AST {
 	class CodeBlock: public AST::Expression {
 		private:
@@ -17,6 +19,10 @@ namespace AST {
 		public:
 		CodeBlock(std::vector<std::unique_ptr<AST::Expression>> body): Expression(body.back()->getType()), body(std::move(body)) {}
 		llvm::Value* codegen(llvm::IRBuilder<> *builder) override;
+		llvm::Constant* codegen_const() override {
+			std::cerr << "codeblock constexpr does not yet evaluate side effects" << std::endl;
+			return body.back()->codegen_const();
+		}
 		void push_before_return(std::unique_ptr<AST::Expression> expr) {
 			body.insert(body.end()-1, std::move(expr));
 		}
